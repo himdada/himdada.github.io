f41eace1985fbe2bde986151d0544a97e4409891
himdada <1173983861@qq.com>
2025-10-21 17:36:50 +0800

    Site updated: 2025-10-21 17:36:49

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["2025/10/12/hello-world/","2025/10/14/himdada的今日摄影/","2025/10/14/himdada的小记/","2025/10/16/今日小记/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
