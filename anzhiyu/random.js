7442d1c2e5fc1f3df34e857b35df92e68cd2719a
himdada <1173983861@qq.com>
2025-10-29 21:10:58 +0800

    Site updated: 2025-10-29 21:10:57

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/14/himdada的今日摄影/","2025/10/14/himdada的小记/","2025/10/21/今日小记/"];function toRandomPost(){
+var posts=["2025/10/14/himdada的今日摄影/","2025/10/14/himdada的小记/","2025/10/21/今日小记/","2025/10/29/今日心情/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
